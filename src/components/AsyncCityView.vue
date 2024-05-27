db6c6d4ce91415a56bfed1ee493df66d4643c893
ralphjasoncadelina <ralphjasoncadelina@gmail.com>
2024-05-27 19:29:19 +0800

    added new component for get req openweathermap api

---
+++ src/components/AsyncCityView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const getWeatherData = async() => {
+    try {
+        const weatherData = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=52b8e4cef2c3d1a7bcfc969fa41fca22&units=imperial`)
+        console.log(weatherData)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+</script>
