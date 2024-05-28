6821591f081ceeecc6b780142cc5598066702697
ralphjasoncadelina <ralphjasoncadelina@gmail.com>
2024-05-28 18:01:36 +0800

    route transitions and page metadata

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <SiteNavigation/>
-    <RouterView/>
+    <RouterView v-slot="{ Component }">
+      <Transition name="page" mode="out-in">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
   </div>
 </template>
 
@@ -12,5 +16,13 @@ import SiteNavigation from './components/SiteNavigation.vue'
 </script>
 
 <style>
+.page-enter-active,
+.page-leave-active {
+  transition: 600mx ease all;
+}
 
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
 </style>
